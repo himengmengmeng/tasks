fb4d5d34ad5866886298cd81c158f4165317a0b0
himengmengmeng <mengartisan@gmail.com>
2025-08-31 16:34:50 +0800

    add core.app related access

--- core/admin.py
+++ core/admin.py
@@ -1,11 +1,31 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin as DefaultUserAdmin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django import forms
 from django.contrib.contenttypes.admin import GenericTabularInline
 from .models import User
 
 
+
+
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name', 'position', 'age', 'is_staff')
+    list_filter = ('is_staff', 'is_superuser', 'is_active', 'position')
+    search_fields = ('username', 'email', 'first_name', 'last_name', 'position')
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'email', 'position', 'age')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'password1', 'password2', 'position', 'age'),
+        }),
+    )
 
 
 
