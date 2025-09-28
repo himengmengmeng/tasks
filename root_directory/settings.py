467873cf91a42772bbd01890dda74bcaa6e1b00d
himengmengmeng <mengartisan@gmail.com>
2025-09-28 16:25:36 +0800

    create_a_goal_app

--- root_directory/settings.py
+++ root_directory/settings.py
@@ -49,11 +49,13 @@ INSTALLED_APPS = [
     'djoser',
     'rest_framework',
     'debug_toolbar',
-    'main_app.apps.MainAppConfig',  # 使用应用配置类而不是字符串
+    'main_app.apps.MainAppConfig',
+    'goal_apps.apps.GoalAppsConfig',
     'core.apps.CoreConfig',
     'django_extensions',
-    'guardian',
+    'guardian', 
 ]
+
 
 
 
